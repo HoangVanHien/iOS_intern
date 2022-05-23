fc235623623ec1343de47d79a0bf94a5886b8f8f
mk1 <mk1@mk1s-Mac-mini.local>
2022-05-23 14:17:24 +0700

    [A] udemy section 5

---
+++ udemy/Magic-8-Ball-iOS13/Magic 8 Ball/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Angela Yu on 14/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    let ballArray:[UIImage?] = [UIImage(named: "ball1"),
+                                UIImage(named: "ball2"),
+                                UIImage(named: "ball3"),
+                                UIImage(named: "ball4"),
+                                UIImage(named: "ball5")]
+
+    @IBAction func onAskButtonPressed(_ sender: Any) {
+        imageView.image = ballArray.randomElement() ?? nil
+    }
+    
+
+}
+
